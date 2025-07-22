6aa1eaba4b3949399f67e8779aaa1797c5e2beac
Justin <42855726+lefortune@users.noreply.github.com>
2025-07-22 16:35:40 +0900

    tilemap testing

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Movement_variables
+    public float moveSpeed = 0.05f;
+    float x_input;
+    float y_input;
+    #endregion
+
+    #region Physics_components
+    Rigidbody2D PlayerRB;
+    SpriteRenderer PlayerSR;
+    BoxCollider2D PlayerColl;
+    public Vector2 currDirection;
+    #endregion
+
+    #region Animation_components
+    Animator anim;
+    #endregion
+
+    #region Other_variables
+    
+    public GameObject pointerPrefab;
+    GameObject interactablePointer;
+    #endregion
+
+    #region Unity_functions
+    private void Awake() {
+        PlayerRB = GetComponent<Rigidbody2D>();
+        PlayerSR = GetComponent<SpriteRenderer>();
+        PlayerColl = GetComponent<BoxCollider2D>();
+        // anim = GetComponent<Animator>();
+    }
+    
+    private void Update() {
+        x_input = Input.GetAxisRaw("Horizontal");
+        y_input = Input.GetAxisRaw("Vertical");
+
+        Move();
+        
+    }
+    #endregion
+
+    #region Movement_functions
+    private void Move()
+    {
+        Vector2 movement = new Vector2(x_input, y_input) * moveSpeed;
+        PlayerRB.MovePosition(PlayerRB.position + movement);
+            moveSpeed = 0.05f;
+            // anim.speed = 1;
+
+        if (movement != Vector2.zero)
+        {
+            currDirection = movement.normalized;
+        }
+
+        // anim.SetFloat("DirX", currDirection.x);
+        // anim.SetFloat("DirY", currDirection.y);
+
+        if (currDirection.x > 0)
+        {
+            PlayerSR.flipX = true;
+        }
+        else if (currDirection.x < 0)
+        {
+            PlayerSR.flipX = false;
+        }
+    }
+    #endregion
+
+    #region Interact_functions
+
+    #endregion
+}
