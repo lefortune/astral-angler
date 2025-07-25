daf833009231c4e22971b572def42d69b19cf715
SabrinaKuang <iamsabrinakuang@berkeley.edu>
2025-07-24 18:35:14 -0700

    dog now doesn't collide with player

--- Assets/Scripts/dogFollow.cs
+++ Assets/Scripts/dogFollow.cs
@@ -3,41 +3,55 @@ using UnityEngine.Tilemaps;
 
 public class dogFollow : MonoBehaviour
 {
+    #region variables
     public Transform playerTransform; 
     public Tilemap path; 
     public float followDistance = 0.5f; 
     public float moveSpeed = 2f; 
+    #endregion
+
+    #region animation
+    private Animator anim;
+    private SpriteRenderer sr;
+    #endregion
+
+    // damn i guess i do need a start fml
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>(); 
+    }
 
     // Update is called once per frame
     void Update()
     {
         followPlayer();
-        
     }
 
     void followPlayer()
     {
-        if (playerTransform == null || path == null)
-        {
-        Debug.LogWarning("Missing references! Make sure playerTransform and path are assigned.");
-        return;
-        }
-
         Vector3 direction = playerTransform.position - transform.position; 
 
         if (direction.magnitude > followDistance)
         {
             Vector3 nextPos = Vector3.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime); 
-            Vector3Int tilePos = path.WorldToCell(nextPos);
-
-            Debug.Log($"Dog trying to move to: {nextPos}, tile: {tilePos}, has tile: {path.HasTile(tilePos)}");
- 
+            Vector3Int tilePos = path.WorldToCell(nextPos); 
 
             if (path.HasTile(tilePos))
             {
                 transform.position = nextPos; 
+
+                if (direction.x < -0.05f)
+                {
+                    sr.flipX = true; // Facing left
+                    anim.SetBool("Walking", true);
+                } 
+                else if (direction.x > 0.05f)
+                {
+                    sr.flipX = false; // Facing right
+                    anim.SetBool("Walking", true);
+                }
             } else {
-                Debug.Log("Next tile is not walkable, dog not moving.");
 
             }
         }
